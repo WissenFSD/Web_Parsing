377c86923f0d37a151360e22fb5edebe9feb692f
wissenfsd@gmail.com <wissenfsd@gmail.com>
2024-04-24 20:40:02 +0300

    sads

--- Web_Parsing/Controllers/HomeController.cs
+++ Web_Parsing/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using HtmlAgilityPack;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Net;
+using System.Text;
 using Web_Parsing.Models;
 
 namespace Web_Parsing.Controllers
@@ -21,13 +22,20 @@ namespace Web_Parsing.Controllers
         }
         public IActionResult MoggleSearch(string search)
         {
+            List<string> location = new List<string>();
             List<string> urls = new List<string>();
             var web = new HtmlWeb();
             var doc = web.Load("https://deprem.afad.gov.tr/last-earthquakes.html");
-            var result =   doc.DocumentNode.SelectNodes("//tr//td");
+            var result =   doc.DocumentNode.SelectNodes("//td[position()=7]");
             foreach (var item in result)
             {
-                
+                Encoding iso = Encoding.GetEncoding("ISO-8859-1");
+                Encoding utf8 = Encoding.UTF8;
+                byte[] utfBytes = utf8.GetBytes(item.InnerText);
+                byte[] isoBytes = Encoding.Convert(utf8, iso, utfBytes);
+                string msg = iso.GetString(isoBytes);
+
+                location.Add(msg);
             }
 
             // Ödev : Ýstediðiniz herhangi bir siteyi parse edip, Xpath kullanarak istediðiniz html'i almanýzý saðlar.Sizde bu örnek üzerinden istediðiniz bir sitenin bir kýsmýný parse edebilirsiniz.
